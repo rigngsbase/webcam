5a9bbc55cfcd7d204fed01189cf2472c87e77d73
Rings Base <ringsbase@gmail.com>
2021-12-04 10:33:28 +0330

    witout dll

--- Form1.cs
+++ Form1.cs
@@ -19,44 +19,40 @@ namespace webcam
 {
     public partial class Form1 : Form
     {
-
         OpenCvSharp.VideoCapture capture;
         OpenCvSharp.Mat frame;
         Bitmap image;
         OpenCvSharp.Mat image1;
         private Thread camera;
         bool isCameraRunning = false;
-
         private void CaptureCamera()
         {
             camera = new Thread(new ThreadStart(CaptureCameraCallback));
             camera.Start();
         }
-
         private void CaptureCameraCallback()
         {
-
             frame = new Mat();
             capture = new VideoCapture(0);
             capture.Open(0);
-
             if (capture.IsOpened())
             {
                 while (isCameraRunning)
                 {
+                    capture.Read(frame);
+                    image1 = frame;
 
-                   capture.Read(frame);
-                     image1 = frame;
-                   // image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
+                    // image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
                     if (pictureBox1.Image != null)
                     {
                         pictureBox1.Image.Dispose();
                     }
-                    pictureBox1.Image =BitmapConverter.ToBitmap(image1);
+                    pictureBox1.Image = BitmapConverter.ToBitmap(image1);
 
-                   
                 }
             }
+
+
         }
 
         public Form1()
@@ -66,9 +62,8 @@ namespace webcam
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+         
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if (button1.Text.Equals("Start"))
@@ -76,6 +71,7 @@ namespace webcam
                 CaptureCamera();
                 button1.Text = "Stop";
                 isCameraRunning = true;
+                
             }
             else
             {
@@ -83,21 +79,19 @@ namespace webcam
                 button1.Text = "Start";
                 isCameraRunning = false;
             }
-
         }
-
         [DllImport("detectcameratest.dll")]
         [return: MarshalAs(UnmanagedType.SafeArray)]
         private extern static string[] ProcessFrame(int width, int height, IntPtr data);
         private void button2_Click(object sender, EventArgs e)
         {
-           
+           // int sleepTime = (int)Math.Round(1000 / capture.Fps);
 
             if (isCameraRunning)
             {
 
-                //while (true)
-                //{
+              //  while (true)
+               // {
                     try
                 {
                         
@@ -105,24 +99,25 @@ namespace webcam
                                           
                     //OpenCvSharp.Mat converted = OpenCvSharp.Extensions.BitmapConverter.ToMat(snapshot);
 
-                    var facelist = ProcessFrame(image1.Width, image1.Height, image1.Data);
+                    var facelist = ProcessFrame(frame.Width, frame.Height, frame.Data);
 
                     foreach (var item in facelist)
                     {
                         listBox1.Items.Add(item);
                     }
-                    Cv2.WaitKey(2);
-
-                  //  snapshot.Save(string.Format(@"C:\Users\Subtek\Desktop\face_datase\{0}.jpg", Guid.NewGuid()), ImageFormat.Jpeg);
-                     // Application.DoEvents();
-                   }
+        
+                   // Thread.Sleep(50);
+                
+                    //  snapshot.Save(string.Format(@"C:\Users\Subtek\Desktop\face_datase\{0}.jpg", Guid.NewGuid()), ImageFormat.Jpeg);
+                   // Application.DoEvents();
+                }
                 //
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex);
                 }
 
-                  //  }
+                   // }
 
             }
 
@@ -130,6 +125,8 @@ namespace webcam
             {
                 MessageBox.Show("Cannot take picture if the camera isn't capturing image!");
             }
+
+            Cv2.WaitKey(0);
 
         }
 
